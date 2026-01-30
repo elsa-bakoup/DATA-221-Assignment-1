ff7f44377adac687c37b0efc886995d45f4ddc0b
elsa-bakoup <elsabakoup1@gmail.com>
2026-01-29 17:45:58 -0700

    Delete Question 1.py

--- Question 1.py
+++
@@ -1,9 +0,0 @@
-threshold = 100
-product = 1
-current_multiplier = 1
-
-while product <= threshold:
-    product *= current_multiplier
-    current_multiplier += 1
-
-print(f"The final product is {product} and the integer that caused the product to exceed the threshold is {current_multiplier-1}.")
